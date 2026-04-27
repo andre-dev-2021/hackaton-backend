3c48d16bc6d605113dca4267c3a6c9898b82002b
andre-dev-2021 <andrepereirasa100@gmail.com>
2026-04-27 14:18:15 -0300

    hotfix

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://koinonia-five.vercel.app/"],
+    allow_origins=["https://koinonia-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
